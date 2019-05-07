2c809dd8fbf592bea67def391efbdef6a76a47b1
Evandro Venancio <evandro.venancio@emanagecrm.com>
2019-05-07 00:54:15 -0300

    Updates

---
+++ precache-manifest.1cfce53f9f9ba10d2d7a2e8b67a519e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb82e081c45a28ce94b655bec77832b",
+    "url": "/enriquereactjstest/index.html"
+  },
+  {
+    "revision": "1b6fe7224ff5c4c20b25",
+    "url": "/enriquereactjstest/static/css/main.22731394.chunk.css"
+  },
+  {
+    "revision": "87e913273ec8e6e3b27a",
+    "url": "/enriquereactjstest/static/js/2.fa9dac84.chunk.js"
+  },
+  {
+    "revision": "1b6fe7224ff5c4c20b25",
+    "url": "/enriquereactjstest/static/js/main.d7cb7d7f.chunk.js"
+  },
+  {
+    "revision": "fc67c7f120d11eb48a38",
+    "url": "/enriquereactjstest/static/js/runtime~main.1feaa74a.js"
+  }
+]);
